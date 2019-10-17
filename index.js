11673d43bde914e0d4f1c1a4dbb7870780cc0c99
radville <laura.radville@gmail.com>
2019-10-17 15:47:44 -0400

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,18 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  const hour = time.split(":")[0]
+  const hourInteger = parseInt(hour, 10)
+  if (hourInteger < 12) {
+    return "Good Morning"
+  } else if (hourInteger <=17 && hourInteger >= 12) {
+    return "Good Afternoon"
+  } else if (hourInteger <= 24 && hourInteger > 17) {
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(message) {
+  document.getElementById("greeting").innerText = message
+}
